92f48e6095f11bf0fa80f464aa16a8d565959c09
Tyler Kirtland <tk@rocketinsights.com>
2019-10-08 10:59:29 -0400

    Add SPM Support

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "TypedNotification",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "TypedNotification", targets: ["TypedNotification"])
+    ],
+    targets: [
+        .target(name: "TypedNotification")
+    ]
+)
+
